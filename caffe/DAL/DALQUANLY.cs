18340bee7fcd27150ba47f67004b4490018c7dd5
raidenmaster <minhtridn0905@gmail.com>
2021-12-19 17:09:37 +0700

    tri add function quan ly ban

---
+++ caffe/DAL/DALQUANLY.cs
@@ -0,0 +1,42 @@
+﻿using QLTV.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caffe.DAL
+{
+    class DALQUANLY
+    {
+        public DataTable LoadBan()
+        {
+            SqlParameter[] p = { };
+            DataTable dt = DB.ExecuteQuery("layBan", p);
+            return dt;
+
+        }
+
+        public bool checkTenBan(string tenban)
+        {
+            try
+            {
+                SqlParameter[] p = {
+            new SqlParameter("@tenban",SqlDbType.NVarChar,50)
+            };
+                p[0].Value = tenban;
+                int rowsAffected = DB.ExecuteNonQuery("dbo.checkTenBan_", p);
+                return rowsAffected == 1;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+
+    }
+}
